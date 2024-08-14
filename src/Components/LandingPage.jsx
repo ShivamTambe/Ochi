3a748ead74cf8de4529e0c4d9f1d13476b7e9a7c
Tambe <stambe@ptc.com>
2024-08-14 14:57:59 +0530

    Landing Page Inital Done

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,24 +1,34 @@
 import React from 'react'
+import { FaArrowUpLong } from "react-icons/fa6";
 
 function LandingPage() {
     return (
         <div className='w-full h-screen bg-zinc-900 pt-1'>
             <div className="textstructure mt-52 px-20">
                 {["We Create", "Eye Opening", "Presentations"].map((item, index) => {
-                    return <div className="masker">
-                        <h1 className="uppercase text-[7.5vw] leading-[6vw] tracking-tighter masker font-['Founders_Grotesk'] font-medium">
-                            {item}
-                        </h1>
+                    return( 
+                    <div className="masker">
+                        <div className='w-fit flex overflow-hidden'>
+                            {index === 1 && (<div className='mr-1vw w-[7.5vw] rounded-md h-[6vw] relative -top-[1vw] bg-red-500'> </div>)}
+                            <h1 className="uppercase text-[7.5vw] leading-[6vw] tracking-tighter masker font-['Founders_Grotesk_X-Condensed'] font-medium">
+                                {item}
+                            </h1>
+                        </div>
                     </div>
+                    )
                 })}
             </div>
             <div className="border-t-2 border-zinc-700 mt-32 flex justify-between items-center py-5 px-20">
                 {["FOr Public and private companies", "From the first pitch to IPO"].map((item, index) => (
                     <p className="text-md font-light tracking-tight leading-none">{item}</p>
                 ))}
-                <div className="start">
-                    <div className="px-5 py-2 border-[2px] rounded-full border-zinc-500 font-light text-sm capitalize">Hello WveryOne</div>
-                    <div className="w-3 h-3 rounded-full"></div>
+                <div className="start flex items-center gap-5">
+                    <div className="px-5 py-2 border-[1px] rounded-full border-zinc-400 font-light text-md uppercase">Hello WveryOne</div>
+                    <div className="w-10 h-10 flex item-center justify-center border-[2px] border-zinc-500 rounded-full">
+                        <span className='rotate-[45deg] ' >
+                            <FaArrowUpLong/>
+                        </span>
+                    </div>
                 </div>
             </div>
             
