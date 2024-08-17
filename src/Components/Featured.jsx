6fecde7d8d31a9a61266d2892a4e210681b9ec7e
Tambe <stambe@ptc.com>
2024-08-17 12:01:09 +0530

    Husshhhh, Animation added

--- src/Components/Featured.jsx
+++ src/Components/Featured.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
-
+import { motion, useAnimation } from 'framer-motion';
+import React, { useState } from 'react'
+import { Power4 } from 'gsap/all';
 function Featured() {
+    const cards = [useAnimation(),useAnimation()];
+    const handleHover =(index)=>{
+        cards[index].start({y:"0"});
+    }
+    const onHoverEnd =(index)=>{
+        cards[index].start({y:"100%"});
+    }
     return (
         <div className='w-full py-20'>
             <div className="w-full px-20 border-b-[1px] border-zinc-700 pb-20">
@@ -8,23 +16,45 @@ function Featured() {
             </div>
             <div className='px-20'>
                 <div className="cards w-full flex gap-10 mt-10">
-                    <div className="cardcontainer relative rounded-xl w-1/2 h-[75vh] bg-red-500 ">
-                        <h1 className="absolute text-[#CDEA68] left-full -translate-x-1/2 top-1/2 -translate-y-1/2 z-[9] text-8xl leading-none tracking-tighter">
-                            <h1 className="absolute text-[#CDEA68] right-full translate-x-1/2 top-1/2 -translate-y-1/2 z-[9] text-8xl leading-none tracking-tighter">
-                                {"VISE".split('').map((item, index) => <span ckas>{item}</span>)}
-                            </h1></h1>
+                    <motion.div 
+                        onHoverStart={()=>handleHover(0)}
+                        onHoverEnd={()=>onHoverEnd(0)}
+                         className="cardcontainer relative rounded-xl w-1/2 h-[75vh] bg-red-500 ">
+                        <div className="absolute text-[#CDEA68] left-full -translate-x-1/2 top-1/2 -translate-y-1/2 z-[9] text-8xl leading-none tracking-tighter">
+                            <h1 className="absolute overflow-hidden flex text-[#CDEA68] right-full translate-x-1/2 top-1/2 -translate-y-1/2 z-[9] text-8xl leading-none tracking-tighter">
+                                {"VISE".split('').map((item, index) => 
+                                <motion.span 
+                                initial={{y:"100%"}} 
+                                animate={cards[0]} 
+                                transition={{ease:[0.22, 1, 0.36, 1],delay:0.05*index}}
+                                className='inline-block'>
+                                    {item}
+                                </motion.span>)}
+                            </h1>
+                        </div>
                         <div className="card w-full h-full rounded-xl overflow-hidden">
                             <img className=" w-full h-full bg-cover " src="https://ochi.design/wp-content/uploads/2024/08/CS_Website_1-663x551.png" alt="" />
                         </div>
-                    </div>
-                    <div className="relative cardcontainer rounded-xl w-1/2 h-[75vh] bg-red-500 ">
+                    </motion.div>
+                    <motion.div 
+                        onHoverStart={()=>handleHover(1)}
+                        onHoverEnd={()=>onHoverEnd(1)}
+                        className="relative cardcontainer rounded-xl w-1/2 h-[75vh] bg-red-500 ">
                         <div className="card w-full h-full rounded-xl overflow-hidden">
-                            <h1 className="absolute text-[#CDEA68] right-full translate-x-1/2 top-1/2 -translate-y-1/2 z-[9] text-8xl leading-none tracking-tighter">
-                                {"VISE".split('').map((item, index) => <span ckas>{item}</span>)}
+                            <h1 className="absolute flex overflow-hidden text-[#CDEA68] right-full translate-x-1/2 top-1/2 -translate-y-1/2 z-[9] text-8xl leading-none tracking-tighter">
+                                {"EDFV".split('').map((item, index) => 
+                                <motion.span 
+                                    initial={{y:"100%"}} 
+                                    animate={cards[1]} 
+                                    transition={{ease:[0.22, 1, 0.36, 1],delay:0.05*index}}
+                                    className='inline-block'>
+                                        {item}
+                                </motion.span>
+                                )}
                             </h1>
                             <img className=" w-full h-full bg-cover " src="https://ochi.design/wp-content/uploads/2024/08/Frame-481692-1-663x551.png" alt="" />
                         </div>
-                    </div>
+                    </motion.div>
                 </div>
             </div>
         </div>
--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,20 +1,29 @@
+import { motion } from 'framer-motion';
 import React from 'react'
 import { FaArrowUpLong } from "react-icons/fa6";
 
 function LandingPage() {
+
     return (
-        <div className='w-full h-screen bg-zinc-900 pt-1'>
+        <div data-scroll data-scroll-section data-scroll-speed="-0.3" className='w-full h-screen bg-zinc-900 pt-1'>
             <div className="textstructure mt-52 px-20">
                 {["We Create", "Eye Opening", "Presentations"].map((item, index) => {
-                    return( 
-                    <div className="masker">
-                        <div className='w-fit flex overflow-hidden'>
-                            {index === 1 && (<div className='mr-1vw w-[7.5vw] rounded-md h-[6vw] relative -top-[1vw] bg-red-500'> </div>)}
-                            <h1 className="uppercase text-[7.5vw] leading-[6vw] tracking-tighter masker font-['Founders_Grotesk_X-Condensed'] font-medium">
-                                {item}
-                            </h1>
+                    return (
+                        <div className="masker">
+                            <div className='w-fit flex overflow-hidden'>
+                                {index === 1 && (
+                                    <motion.div
+                                        initial={{ width: 0 }}
+                                        animate={{ width: "9vw" }}
+                                        transition={{ ease: [0.76, 0, 0.24, 1], duration:1 }}
+                                        className='mr-1vw w-[7.5vw] rounded-md h-[6vw] relative -top-[1vw] bg-red-500'>
+                                    </motion.div>)
+                                }
+                                <h1 className="uppercase text-[7.5vw] leading-[6vw] tracking-tighter masker font-['Founders_Grotesk_X-Condensed'] font-medium">
+                                    {item}
+                                </h1>
+                            </div>
                         </div>
-                    </div>
                     )
                 })}
             </div>
@@ -26,12 +35,12 @@ function LandingPage() {
                     <div className="px-5 py-2 border-[1px] rounded-full border-zinc-400 font-light text-md uppercase">Hello WveryOne</div>
                     <div className="w-10 h-10 flex item-center justify-center border-[2px] border-zinc-500 rounded-full">
                         <span className='rotate-[45deg] ' >
-                            <FaArrowUpLong/>
+                            <FaArrowUpLong />
                         </span>
                     </div>
                 </div>
             </div>
-            
+
         </div>
     )
 }
